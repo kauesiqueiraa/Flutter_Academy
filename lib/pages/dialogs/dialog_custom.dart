c3de5b0ce970b2890172a1fd08dfe198eeeb8d1e
kauesiqueiraa <kauesiqueira_@hotmail.com>
2024-10-15 08:18:32 -0300

    feat: created Dialogs functions

--- lib/pages/dialogs/dialog_custom.dart
+++ lib/pages/dialogs/dialog_custom.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
 class DialogCustom extends Dialog{
-  DialogCustom(): super(
+  DialogCustom(BuildContext context): super(
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(12),
     ),
     child: Container(
       width: 300,
       height: 300,
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Titulo X'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(), 
+            child: const Text('Fechar Dialog'),
+          )
+        ],
+      ),
     ),
   );
 }
--- lib/pages/dialogs/dialogs_page.dart
+++ lib/pages/dialogs/dialogs_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_primeiro_projeto/pages/dialogs/dialog_fluttermaonamassa.dart';
+import 'package:flutter_primeiro_projeto/pages/dialogs/dialog_custom.dart';
 
 class DialogsPage extends StatelessWidget {
 const DialogsPage({ super.key });
@@ -13,33 +13,110 @@ const DialogsPage({ super.key });
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  // barrierDismissible: false,
+              onPressed: () async {
+                await showDialog(
+                  barrierDismissible: false,
                   context: context, 
-                  builder: (context) {
-                    return DialogCustom();
+                  builder: (_) {
+                    return DialogCustom(context);
                   }
                 );
               },
               child: const Text('Dialog'),
             ),
             ElevatedButton(
-              onPressed: () {},
-              child: Text('Simple Dialog'),
+              onPressed: () async {
+                await showDialog(
+                  context: context, 
+                  builder: (context) {
+                    return SimpleDialog(
+                      title: const Text('SimpleDialog'),
+                      contentPadding: const EdgeInsets.all(10.0),
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text('Titulo Y'),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text('Fechar Dialog'),
+                        )
+                      ],
+                    );
+                  }
+                );
+              },
+              child: const Text('Simple Dialog'),
             ),
             ElevatedButton(
-              onPressed: () {},
-              child: Text('Alert Dialog'),
+              onPressed: () async {
+                await showDialog(
+                  context: context, 
+                  builder: (context){
+                    return AlertDialog(
+                      title: const Text("Alert Dialog"),
+                      content: const SingleChildScrollView(
+                        child: ListBody(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: Text('Deseja realmente Salvar????'),
+                            ),
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text('Cancelar'),
+
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text('Salvar'),
+                        ),
+                      ],
+                    );
+                  });
+              },
+              child: const Text('Alert Dialog'),
             ),
             ElevatedButton(
-              onPressed: () {},
-              child: Text('Time Picker'),
+              onPressed: () async {
+                final selectedTime = await showTimePicker(
+                  context: context, 
+                  initialTime: TimeOfDay.now(),
+                );
+                print('Selected Time: $selectedTime');
+              },
+              child: const Text('Time Picker'),
             ),
             ElevatedButton(
-              onPressed: () {},
-              child: Text('Date Picker'),
+              onPressed: () async {
+                final selectedDate = await showDatePicker(
+                  context: context, 
+                  firstDate: DateTime(2000), 
+                  lastDate: DateTime(2030),
+                );
+                print('A data selecionado foi $selectedDate');
+              },
+              child: const Text('Date Picker'),
             ),
+            ElevatedButton(
+              onPressed: (){
+                showAboutDialog(
+                  context: context,
+                  applicationVersion: '1.0.0',
+                  applicationIcon: const Icon(Icons.flutter_dash),
+                  children: [
+                    const Text('Desenvolvedor'),
+                    const Text('Desenvolvedor'),
+                    const Text('Projeto'),
+                  ],
+                );
+              }, 
+              child: const Text('About Dialog Version')
+            )
           ],
         )
       ),
